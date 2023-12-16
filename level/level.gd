510335e29cc6978a04502c2f0313cfcbfbbfffd5
Dominik Pokorný <dommterr@gmail.com>
2023-12-16 19:26:38 +0100

    added explosion scene

--- level/level.gd
+++ level/level.gd
@@ -3,3 +3,5 @@ extends Node2D
 func _process(_delta):
 	if Input.is_key_pressed(KEY_Q) == true:
 		GameManager.load_main_scene()
+	if Input.is_key_pressed(KEY_E) == true:
+		ObjectMaker.create_explosion(Vector2(100, 200))
--- level_bg/level_bg.gd
+++ level_bg/level_bg.gd
@@ -8,10 +8,10 @@ const SPEED: float = 200.0
 
 
 func _process(delta):
-    parallax_layer_1.motion_offset.y += SPEED * delta * 0.2
-    parallax_layer_2.motion_offset.y += SPEED * delta * 0.3
-    parallax_layer_3.motion_offset.y += SPEED * delta * 0.4
+	parallax_layer_1.motion_offset.y += SPEED * delta * 0.2
+	parallax_layer_2.motion_offset.y += SPEED * delta * 0.3
+	parallax_layer_3.motion_offset.y += SPEED * delta * 0.4
 
 
 func set_running(running: bool) -> void:
-    set_process(running)
+	set_process(running)
