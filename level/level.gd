37576c914aedaead0a0988ad84d9154f9f5bf780
Dominik Pokorný <dommterr@gmail.com>
2023-12-16 19:32:14 +0100

    added boom scene

--- level/level.gd
+++ level/level.gd
@@ -5,3 +5,5 @@ func _process(_delta):
 		GameManager.load_main_scene()
 	if Input.is_key_pressed(KEY_E) == true:
 		ObjectMaker.create_explosion(Vector2(100, 200))
+	if Input.is_key_pressed(KEY_B) == true:
+		ObjectMaker.create_boom(Vector2(200, 100))
