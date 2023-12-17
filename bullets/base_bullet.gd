1e2201bc59bbb475b371bbbf1a844d5714a8d9f6
Dominik Pokorný <dommterr@gmail.com>
2023-12-17 19:03:29 +0100

    bullet scenes created

---
+++ bullets/base_bullet.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+var direction: Vector2 = Vector2.UP
+var speed: float = 200.0
+var damage: int = 10
+
+
+func _ready():
+	pass
+	
+
+func _process(delta):
+	position += direction * speed * delta
+
+
+func setup(pos: Vector2, dir: Vector2, sp: float, dmg: int) -> void:
+	direction = dir
+	speed = sp
+	damage = dmg
+	global_position = pos
+
+
+func blow_up(area: Node2D) -> void:
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
+func _on_area_entered(area):
+	blow_up(area)
