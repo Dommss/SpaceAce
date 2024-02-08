39431d524f7268bd30681b47d4ba4f69b78d2f9d
Dominik Pokorný <dommterr@gmail.com>
2024-02-08 12:32:00 +0100

    instantiating explosions at correct pos

--- level/level.gd
+++ level/level.gd
@@ -3,7 +3,3 @@ extends Node2D
 func _process(_delta):
 	if Input.is_key_pressed(KEY_Q) == true:
 		GameManager.load_main_scene()
-	if Input.is_key_pressed(KEY_E) == true:
-		ObjectMaker.create_explosion(Vector2(100, 200))
-	if Input.is_key_pressed(KEY_B) == true:
-		ObjectMaker.create_boom(Vector2(200, 100))
